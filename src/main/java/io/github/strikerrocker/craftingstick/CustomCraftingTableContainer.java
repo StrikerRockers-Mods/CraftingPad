77b790bae19a4b33011257fa96f2c74e0d17f13a
Tharik Sherief <thariksherief1@gmail.com>
2019-04-25 07:12:08 +0530

    Update to 1.14 and 0.4 loader

--- src/main/java/io/github/strikerrocker/craftingstick/CustomCraftingTableContainer.java
+++ src/main/java/io/github/strikerrocker/craftingstick/CustomCraftingTableContainer.java
@@ -1,16 +1,15 @@
-package io.github.strikerrocker.craftingstick;
+package io.github.strikerrocker.craftingstick
+        ;
 
-import net.minecraft.class_3914;
+import net.minecraft.container.BlockContext;
 import net.minecraft.container.CraftingTableContainer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class CustomCraftingTableContainer extends CraftingTableContainer {
 
-    public CustomCraftingTableContainer(int int1, PlayerInventory var1, class_3914 class_3914) {
-        super(int1, var1,class_3914);
+    public CustomCraftingTableContainer(int int1, PlayerInventory var1, BlockContext blockContext) {
+        super(int1, var1, blockContext);
     }
 
     @Override
--- src/main/java/io/github/strikerrocker/craftingstick/ItemCraftingStick.java
+++ src/main/java/io/github/strikerrocker/craftingstick/ItemCraftingStick.java
@@ -1,14 +1,9 @@
 package io.github.strikerrocker.craftingstick;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.class_3914;
 import net.minecraft.client.network.ClientDummyContainerProvider;
-import net.minecraft.container.Container;
-import net.minecraft.container.CraftingTableContainer;
+import net.minecraft.container.BlockContext;
 import net.minecraft.container.NameableContainerProvider;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.TextComponent;
@@ -30,12 +25,12 @@ public class ItemCraftingStick extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World var1, PlayerEntity var2, Hand var3) {
         if (!var1.isClient()) {
-            var2.openContainer(method_17454(var1,var2.getPos()));
+            var2.openContainer(method_17454(var1, var2.getBlockPos()));
         }
         return new TypedActionResult<>(ActionResult.PASS, var2.getStackInHand(var3));
     }
 
     public NameableContainerProvider method_17454(World world_1, BlockPos blockPos_1) {
-        return new ClientDummyContainerProvider((int_1, playerInventory_1, playerEntity_1) -> new CustomCraftingTableContainer(int_1, playerInventory_1, class_3914.method_17392(world_1, blockPos_1)), field_17362);
+        return new ClientDummyContainerProvider((int_1, playerInventory_1, playerEntity_1) -> new CustomCraftingTableContainer(int_1, playerInventory_1, BlockContext.create(world_1, blockPos_1)), field_17362);
     }
 }
