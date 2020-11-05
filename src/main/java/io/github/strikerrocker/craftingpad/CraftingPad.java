971b347d0141b9f8860e5b88e6ae8cd0785f6bbd
Guntram Blohm <gbl@guntram.de>
2020-11-05 14:52:27 +0100

    update to 1.16.4, add crowdin

--- src/main/java/io/github/strikerrocker/craftingpad/CraftingPad.java
+++ src/main/java/io/github/strikerrocker/craftingpad/CraftingPad.java
@@ -1,18 +1,23 @@
 package io.github.strikerrocker.craftingpad;
 
+import de.guntram.mcmod.crowdintranslate.CrowdinTranslate;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
 
 public class CraftingPad implements ModInitializer {
+    
+    public static final String MODID = "craftingpad";
+
     public static Item registerItem(Item item, String name) {
-        Registry.register(Registry.ITEM, "craftingpad:" + name, item);
+        Registry.register(Registry.ITEM, MODID + ":" + name, item);
         return item;
     }
 
     @Override
     public void onInitialize() {
-        registerItem(new ItemCraftingPad(new Item.Settings().group(ItemGroup.MISC)), "craftingpad");
+        registerItem(new ItemCraftingPad(new Item.Settings().group(ItemGroup.MISC)), MODID);
+        CrowdinTranslate.downloadTranslations(MODID);
     }
 }
